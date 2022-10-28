665ec764e062bf3ee875d6c472525e61284e68e4
unknown <brunnowr.bw@gmail.com>
2022-10-29 00:08:27 +0200

    implementações de funções via javascript

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// ativar links da nav
+
+const links = document.querySelectorAll('.header-menu a');
+function ativarLink(link) {
+  const url = location.href
+  const href = link.href
+  if (url.includes(href)) {
+    link.classList.add('ativo')
+  }
+}
+
+links.forEach(ativarLink)
+
+// ATIVAR INTEMS DO ORÇAMENTO 
+
+const parametros = new URLSearchParams(location.search) 
+
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro)
+
+  if (elemento) {
+    elemento.checked = true
+  }
+
+  
+}
+
+parametros.forEach(ativarProduto)
